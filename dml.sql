12a00d3f2ee4e704e6d0a605ab2091fc150e7653
mn1490949269 <1490949269@qq.com>
2019-08-09 10:39:50 +0800

    Update dml.sql

--- dml.sql
+++ dml.sql
@@ -1,7 +1,8 @@
 -- 插入记录  
-
+INSERT INTO student(id,name,age,sex) VALUES('003','王五','10','男');
 -- 修改记录  
-
+UPDATE student SET age='15' WHERE id='003';
 --  删除记录  
-
+delete from student WHERE id='003';
 -- 查询记录  
+select * from student;
